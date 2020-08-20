c03af2a3725e27df11cc83a868c0846cb1b5d867
gimsangjun <43605835+gimsangjun@users.noreply.github.com>
2020-08-21 00:38:52 +0900

    adding "main_alarm" function

--- function.py
+++ function.py
@@ -32,26 +32,30 @@ def get_bs_obj(company_code):
 
 
 # 내가받아온 웹페이즈 소스코드중에서 내가 가져와야할 가격을 가져옴
-def get_price(company_code):
+def get_price(company):
 
+    company_code = get_company_code(company)
     # 현재가격
     bs_obj = get_bs_obj(company_code)
     no_today = bs_obj.find("p", {"class": "no_today"})
     now_price = no_today.find("span", {"class": "blind"}).text
+    now_price = now_price.replace(",", "")
 
     # 시가
     today = bs_obj.find_all("td", {"class": "first"})
     today = today[1]
     today_price = today.find("span", {"class": "blind"}).text
+    today_price = today_price.replace(",", "")
 
     # 전일가격
     yesterday = bs_obj.find("td", {"class": "first"})
     yesterday_price = yesterday.find("span", {"class": "blind"}).text
+    yesterday_price = yesterday_price.replace(",", "")
 
     return {
-        "now_price": now_price,
-        "today_price": today_price,
-        "yesterday_price": yesterday_price,
+        "현재가": now_price,
+        "시초가": today_price,
+        "전일가": yesterday_price,
     }
 
 
@@ -72,13 +76,10 @@ def stock_exist(company):
         exit(1)  # 그냥종료시켜보리기
 
 
-# 내 주식에 종목추가하기
+# 내 주식에 종목추가하기 , 이미 존재하는 종목이면 수정 할수 있는 기능.
 def plus_mystock(company, price, count):
 
-    # company = input("추가하고싶은 종목의 이름을 넣어주세요\n")
     stock_exist(company)
-    # price = input("가격을 입력해주세요\n")
-    # count = input("갯수를 입력해주세요\n")
     total_price = int(price) * int(count)
     filename = r"C:\Users\kimsangjun\Documents\coding\test\my_stock.xlsx"
     wb = openpyxl.load_workbook(filename)
@@ -89,5 +90,51 @@ def plus_mystock(company, price, count):
     wb.save(filename)
 
 
-# 전일대비 얼마나 올랐나,생각해보니 이거는 만들필요가 없었음. 현재가격이랑 전일가격만 있으면 그냥 계산할수있는거였음. 얻을수 있는 교훈 어떻게 만들지 먼저 생각을 해야한다.
+# 어떤 가격에 도달하면 알람을 주는지 추가
+# stock_alarm실행중, 이미 존재하는 종목을 추가하는 경우. 이대로 추가할것인지 , 수정할것인지 물어보는 기능을 넣어보는게 좋을듯.
+def stock_alarm(company, buy_price, sell_price):
+    stock_exist(company)
+    filename = r"C:\Users\kimsangjun\Documents\coding\test\alarm.xlsx"
+    gp = get_price(company)  # 가격을 불러옴/
+    now_price = gp[1]  # gp[1]에 해당하는게 현재가격이기 때문에 이런식으로 해줬음.
+    wb = openpyxl.load_workbook(filename)
+    sheet = wb.active
+    sheet.append([company, now_price, buy_price, sell_price])
+    wb.save(filename)
 
+
+def main_alarm():
+
+    df = pd.read_excel(
+        r"C:\Users\kimsangjun\Documents\coding\test\alarm.xlsx",
+        dtype={"현재가격": int, "매수가격알림": int, "매도가격알림": int},
+    )
+
+    # 어떻게 엑셀을 불러와서 현재가격을 계속초기화할 생각을 하고있엇는데 그렇게 할 필요가 없을거 같음. 실시간으로 현재가격을 가져와야하기때문에, 엑셀에 현재가격을 표시할 이유가 없을듯.
+
+    lists = df.values.tolist()
+    message = []
+    for i in range(len(lists)):  # list : [회사명,현재가격,매수가,매도가]
+        company = lists[i][0]  # 회사명 할당
+        price = get_price(company)  # 현재가격 할당
+        now_price = price["현재가"]  # get_price가 딕셔너리형태로 반환을 해주기 떄문
+        now_price = int(now_price)  # 딕셔너리형태라 그런지 str으로 되어있어서 int형으로 바꿔줌.
+        # 계속 현대차 오류나길래 봣더니 엑셀안에서는 현대자동차로 되어있음.
+
+        if int(now_price) == lists[i][2]:
+            # print(list[0] + " 이/가 " + str(now_price) + "원(매수 가격)에 도달하였습니다.")
+            mes = company + " 이/가 " + str(now_price) + "원(매수 가격)에 도달하였습니다."
+            message.append(mes)
+        elif int(now_price) == lists[i][3]:
+            # print(list[0] + " 이/가 " + str(now_price) + "원(매도 가격)에 도달하였습니다.")
+            mes = company + " 이/가 " + str(now_price) + "원(매도 가격)에 도달하였습니다."
+            message.append(mes)
+    return message
+
+
+# message = [] 이렇게 선언하고, 바로 message[0] = 123 이런식으로 할당할려고 했는데 자꾸 아웃오브 인덱싱 오류가떠서 append로 하니 해결댐.
+# 그리고 미리 message = [1,2,3] 이런식으로 선언하고, message[0]이런식으로 하니까 되긴함. , [] 이렇게 선언했을댸 인덱스가 아무것도 없어서 그런가봄.
+
+
+# 전일대비 얼마나 올랐나,생각해보니 이거는 만들필요가 없었음. 현재가격이랑 전일가격만 있으면 그냥 계산할수있는거였음. 얻을수 있는 교훈 어떻게 만들지 먼저 생각을 해야한다.
+print(main_alarm())
--- run.py
+++ run.py
@@ -1,10 +1,17 @@
 from discord.ext import commands
 import function as f  # 내가 기존에 만든 웹스크래핑 장치들
+import discord, os, asyncio  # 토큰값을 숨기기위해
 
 bot = commands.Bot(
     command_prefix="!"
 )  # prefix를 가지고있을때 명령어를 칠 때는 저 prefix가 꼭 앞에 있어야 한다.
 
+token_path = os.path.dirname(os.path.abspath(__file__)) + "/token.txt"
+t = open(r"C:\Users\kimsangjun\Documents\coding\test\token.txt", "r", encoding="utf-8")
+token = t.read().split()[0]
+print("Token_key : ", token)
+
+# game = discord.Game("!도움")
 """
 # @이거는 뜻이 뭘까?
 @bot.command()
@@ -29,6 +36,18 @@ async def upper(ctx, content: to_upper):
 """
 
 
+@bot.event
+async def on_ready():
+    print("봇 시작")
+
+
+@bot.command()
+async def 도움(ctx):
+    await ctx.send("명령어 : !가격 회사명 - 현재가, 시초가 ,전일가격을 알수있습니다.")
+    await ctx.send("명령어 : !종목추가 회사명 가격 갯수 - 아직 미완성입니다.")
+    await ctx.send("명령어 : !종목알람추가 회사명 매수가격 매도가격 - 그 가격에 오면 매수알람,매도알람이 오게 해줍니다.")
+
+
 @bot.command()
 async def 종목추가(ctx, company, price, count):
     f.plus_mystock(company, price, count)
@@ -36,9 +55,29 @@ async def 종목추가(ctx, company, price, count):
 
 @bot.command()
 async def 가격(ctx, company):  # 현재가 시초가 어제가격
-    company_code = f.get_company_code(company)
-    price_list = f.get_price(company_code)
-    await ctx.send(price_list)
+    price_list = f.get_price(company)
+    for company, price in price_list.items():
+        message = company + " : " + price
+        await ctx.send(message)
 
 
-bot.run("")
+@bot.command()
+async def 종목알람추가(ctx, company, buy_price, sell_price):
+    f.stock_alarm(company, buy_price, sell_price)
+
+    df = f.stock_alarm_list()
+    # 이부분 수정필요.
+    if company in df:
+        await ctx.send("이미 존재하는 종목입니다.")
+    else:
+        exit(1)  # 그냥종료시켜보리기
+
+
+@bot.command()
+async def 알람(ctx):
+    message = f.main_alarm()
+    if message != 1:  # 매수가격 도달
+        await ctx.send(message)
+
+
+bot.run(token)
